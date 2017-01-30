dafd9d5d2ad67ae0002b26009d6179cd621fd4b2
Adriel Fabella <adriel.fab@gmail.com>
2017-01-30 17:49:38 -0500

    next

--- app/src/main/java/com/example/lecture/Main2Activity.java
+++ app/src/main/java/com/example/lecture/Main2Activity.java
@@ -1,5 +1,6 @@
 package com.example.lecture;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,7 +13,9 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        String name = getIntent().toString();
+        mTextView = (TextView) findViewById(R.id.textView3);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
         mTextView.setText(name);
 
     }
--- app/src/main/java/com/example/lecture/MainActivity.java
+++ app/src/main/java/com/example/lecture/MainActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mButton = (Button) findViewById(R.id.button);
+        mEditText = (EditText) findViewById(R.id.editText);
 
         mButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                intent.putExtra("name", "adriel");
-                startActivity(intent);
+                String name = mEditText.getText().toString();
+                change(name);
             }
         });
+    }
+
+    private void change(String name){
+        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+        intent.putExtra("name", name);
+        startActivity(intent);
     }
 }
