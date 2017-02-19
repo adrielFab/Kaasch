72f9447f994e8905e840c671d4d344e9934448ad
arossergit config --global user.name arosser <arosser95@gmail.com>
2017-02-18 22:13:06 -0500

    IK-90 Changed minimum Sdk to 16, updates to home screen

--- app/src/main/java/com/example/mrides/MainActivity.java
+++ app/src/main/java/com/example/mrides/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.mrides;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** got to sign in page */
+    public void goToSignIn(View view) {
+        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+
+
+
+
 }
