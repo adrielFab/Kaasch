dafd0b3d1b6c5d87f6b9d9bcb6f98a0a46ab54b3
Adriel Fabella <adriel.fab@gmail.com>
2017-02-23 08:28:29 -0500

    IK-49 solved : icon and polyline not appearing

--- app/src/main/java/com/example/mrides/CreateRouteActivity.java
+++ app/src/main/java/com/example/mrides/CreateRouteActivity.java
@@ -1,6 +1,8 @@
 package com.example.mrides;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,6 +22,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
@@ -34,6 +38,7 @@ public class CreateRouteActivity extends FragmentActivity implements OnMapReadyC
     private EditText mEditTextStart;
     private EditText mEditTextDestination;
     private GoogleMap mGoogleMap;
+    private ProgressDialog mProgressDialog;
     private List<Marker> startMarkers = new ArrayList<>();
     private List<Marker> destinationMarkers = new ArrayList<>();
     private List<Polyline> polylinePaths = new ArrayList<>();
@@ -45,6 +50,8 @@ public class CreateRouteActivity extends FragmentActivity implements OnMapReadyC
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         mButtonFindPath = (Button) findViewById(R.id.buttonFindPath);
         mEditTextStart = (EditText) findViewById(R.id.editTextStart);
         mEditTextDestination = (EditText) findViewById(R.id.editTextDestination);
@@ -70,10 +77,17 @@ public class CreateRouteActivity extends FragmentActivity implements OnMapReadyC
             return;
         }
 
+        try{
+            new ObtainDirection(this, start, destination).execute();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
         int height = 100;
         int width = 100;
         BitmapDrawable bitmapdraw=(BitmapDrawable)getResources().getDrawable(R.drawable.passenger_icon);
@@ -90,11 +104,59 @@ public class CreateRouteActivity extends FragmentActivity implements OnMapReadyC
 
     @Override
     public void startObtainDirection() {
-        //progress loading
+        mProgressDialog = ProgressDialog.show(this, "Please wait.",
+                "Finding direction...", true);
+
+        if (startMarkers != null) {
+            for (Marker marker : startMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (destinationMarkers != null) {
+            for (Marker marker : destinationMarkers) {
+                marker.remove();
+            }
+        }
+
+        if (polylinePaths != null) {
+            for (Polyline polyline:polylinePaths ) {
+                polyline.remove();
+            }
+        }
     }
 
     @Override
-    public void successObtainDirection(List<Route> route) {
-        //display route
+    public void successObtainDirection(List<Route> routes) {
+        mProgressDialog.dismiss();
+        polylinePaths = new ArrayList<>();
+        startMarkers = new ArrayList<>();
+        destinationMarkers = new ArrayList<>();
+
+        for (Route route : routes) {
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(route.startLocation, 16));
+            ((TextView) findViewById(R.id.textDuration)).setText(route.duration.text);
+            ((TextView) findViewById(R.id.textDistance)).setText(route.distance.text);
+
+            startMarkers.add(mGoogleMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+                    .title(route.startAddress)
+                    .position(route.startLocation)));
+
+            destinationMarkers.add(mGoogleMap.addMarker(new MarkerOptions()
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                    .title(route.endAddress)
+                    .position(route.endLocation)));
+
+            PolylineOptions polylineOptions = new PolylineOptions(). //Route
+                    geodesic(true).
+                    color(Color.CYAN).
+                    width(10);
+
+            for (int i = 0; i < route.points.size(); i++)
+                polylineOptions.add(route.points.get(i));
+
+            polylinePaths.add(mGoogleMap.addPolyline(polylineOptions));
+        }
     }
 }
