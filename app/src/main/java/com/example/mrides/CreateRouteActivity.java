a69eaa551b612df1b4e91a81d8757f0a3611c257
Adriel Fabella <adriel.fab@gmail.com>
2017-02-22 23:30:37 -0500

    IK-49 Added the corresponding fields and classes

--- app/src/main/java/com/example/mrides/CreateRouteActivity.java
+++ app/src/main/java/com/example/mrides/CreateRouteActivity.java
@@ -2,12 +2,24 @@ package com.example.mrides;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateRouteActivity extends AppCompatActivity {
+
+    private Button btnFindPath;
+    private EditText etStart;
+    private EditText etDestination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_route);
+
+        btnFindPath = (Button) findViewById(R.id.btnFindPath);
+        etStart = (EditText) findViewById(R.id.etStart);
+        etDestination = (EditText) findViewById(R.id.etDestination);
+
+        
     }
 }
