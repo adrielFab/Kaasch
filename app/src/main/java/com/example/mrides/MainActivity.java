a237fd16b2b7293330462edb466dd79c36720c47
Harrison <harrison.a101@hotmail.com>
2017-02-23 16:58:38 -0500

    IK-47 Users login straight from the main page.

--- app/src/main/java/com/example/mrides/MainActivity.java
+++ app/src/main/java/com/example/mrides/MainActivity.java
@@ -20,19 +20,72 @@ import com.google.android.gms.common.api.Status;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
+
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Request only the user's ID token, which can be used to identify the
+        // user securely for the backend. This will contain the user's basic
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_client_id))
+                .build();
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, (GoogleApiClient.OnConnectionFailedListener) this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
     }
 
-    public void SignInPage(View view){
-        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
-        startActivity(intent);
+    // onclick event for sign in button
+    public void googleSignIn(View view) {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    //result from the sign in
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            String idToken = acct.getIdToken();
+            SignInRequestHandler signInRequestHandler = new SignInRequestHandler();
+            signInRequestHandler.authenticateGoogleAccount(this,idToken);
+            signInRequestHandler.logInUser(this);
+            System.out.println("inside");
+            Intent intent = new Intent(MainActivity.this, TempMainActivity.class);
+            startActivity(intent);
+            //signInRequestHandler.testServer(this);
+        } else {
+            // Signed out, show unauthenticated UI.
+            System.out.println("Failed");
+            //updateUI(false);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        //TODO message must show up if gmail user account could not be optained
+        //most likely related to no wifi connection
     }
 
 
