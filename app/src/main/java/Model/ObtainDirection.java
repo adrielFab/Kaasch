4095a4972646f993e34ce14fcad282873971bb59
Adriel Fabella <adriel.fab@gmail.com>
2017-02-22 23:36:22 -0500

    Added the google map fragment

--- app/src/main/java/Model/ObtainDirection.java
+++ app/src/main/java/Model/ObtainDirection.java
@@ -1,4 +1,14 @@
 package Model;
 
 public class ObtainDirection {
+
+    private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?";
+    private static final String GOOGLE_API_KEY = "AIzaSyDnwLF2-WfK8cVZt9OoDYJ9Y8kspXhEHfI";
+    private String start;
+    private String destination;
+
+    public ObtainDirection(String start, String destination){
+        this.start = start;
+        this.destination = destination;
+    }
 }
--- app/src/main/java/com/example/mrides/CreateRouteActivity.java
+++ app/src/main/java/com/example/mrides/CreateRouteActivity.java
@@ -1,11 +1,16 @@
 package com.example.mrides;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class CreateRouteActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class CreateRouteActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private Button btnFindPath;
     private EditText etStart;
@@ -16,10 +21,17 @@ public class CreateRouteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_route);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
         btnFindPath = (Button) findViewById(R.id.btnFindPath);
         etStart = (EditText) findViewById(R.id.etStart);
         etDestination = (EditText) findViewById(R.id.etDestination);
 
-        
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
