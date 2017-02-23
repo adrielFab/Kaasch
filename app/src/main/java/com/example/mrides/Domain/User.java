39515f6a9b854f30919905a04b8f8dbc4d99f6cb
Harrison <harrison.a101@hotmail.com>
2017-02-22 23:39:58 -0500

    IK-47 added a user java class and added ip to resource folder.

--- app/src/main/java/com/example/mrides/Domain/User.java
+++ app/src/main/java/com/example/mrides/Domain/User.java
@@ -1,8 +1,6 @@
 package com.example.mrides.Domain;
 
-/**
- * Created by Deez Nuts on 2/22/2017.
- */
+
 
 public class User {
 
@@ -12,4 +10,44 @@ public class User {
     private String givenName;
     private String familyName;
 
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public void setGivenName(String givenName) {
+        this.givenName = givenName;
+    }
+
+    public String getProfilePictureUrl() {
+        return profilePictureUrl;
+    }
+
+    public void setProfilePictureUrl(String profilePictureUrl) {
+        this.profilePictureUrl = profilePictureUrl;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
 }
--- app/src/main/java/com/example/mrides/SignInRequestHandler.java
+++ app/src/main/java/com/example/mrides/SignInRequestHandler.java
@@ -6,14 +6,17 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.mrides.Domain.User;
+import com.google.gson.Gson;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
 public class SignInRequestHandler {
 
-    private String url = "http://192.168.0.154:80";
-    boolean validAccout = false; // flag for authentic google account
+    private boolean validAccout = false; // flag for authentic google account
+    private User user;
 
     public boolean authenticateGoogleAccount(Context context,String clientId){
         // Request a string response from the provided URL.
@@ -40,11 +43,17 @@ public class SignInRequestHandler {
     public void logInUser(Context context){
 
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
-                (Request.Method.GET, url+"/login.php", null, new Response.Listener<JSONObject>() {
+                (Request.Method.POST, context.getString(R.string.url)+"/login.php", null, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
                         System.out.println("Response: " + response.toString());
+                        /*Gson gson = new Gson();
+                        try {
+                            user = gson.fromJson(response.getJSONObject("user").toString(), User.class);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }*/
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -53,7 +62,6 @@ public class SignInRequestHandler {
                     }
                 });
         RequestQueueSingleton.getInstance(context).addToRequestQueue(jsObjRequest);
-
     }
 
 
