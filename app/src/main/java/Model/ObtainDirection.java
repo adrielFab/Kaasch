486269fc49b345f41b06b562e17d95455b4717e5
Adriel Fabella <adriel.fab@gmail.com>
2017-02-22 23:39:11 -0500

    IK-49 Encoded the start and end to accomodate the appropriate URL format

--- app/src/main/java/Model/ObtainDirection.java
+++ app/src/main/java/Model/ObtainDirection.java
@@ -1,5 +1,8 @@
 package Model;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class ObtainDirection {
 
     private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?";
@@ -10,5 +13,11 @@ public class ObtainDirection {
     public ObtainDirection(String start, String destination){
         this.start = start;
         this.destination = destination;
+    }
+
+    private String createUrl() throws UnsupportedEncodingException {
+        String urlOrigin = URLEncoder.encode(start, "utf-8");
+        String urlDestination = URLEncoder.encode(destination, "utf-8");
+        return DIRECTION_URL_API + "origin=" + urlOrigin + "&destination=" + urlDestination + "&key=" + GOOGLE_API_KEY;
     }
 }
