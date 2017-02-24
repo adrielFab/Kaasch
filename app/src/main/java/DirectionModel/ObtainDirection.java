dc160348ae9a2bf524cca813c5cb506f9847cd51
Adriel Fabella <adriel.fab@gmail.com>
2017-02-24 13:55:40 -0500

    Modified API key

--- app/src/main/java/DirectionModel/ObtainDirection.java
+++ app/src/main/java/DirectionModel/ObtainDirection.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class ObtainDirection {
 
     private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?";
-    private static final String GOOGLE_API_KEY = "AIzaSyDnwLF2-WfK8cVZt9OoDYJ9Y8kspXhEHfI";
+    private static final String GOOGLE_API_KEY = "AIzaSyDBjGOW7Yv5t-RwJI-Dhl86rZnZcQCd93c";
     private ObtainDirectionListener listener;
     private String start;
     private String destination;
