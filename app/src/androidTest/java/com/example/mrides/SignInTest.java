b5299af68c2bf81711c9545e6977e68a0cfb7265
Harrison <harrison.a101@hotmail.com>
2017-02-23 22:35:20 -0500

    IK-48 added test file and test dependencies in gradle file.

---
+++ app/src/androidTest/java/com/example/mrides/SignInTest.java
@@ -0,0 +1,29 @@
+package com.example.mrides;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class SignInTest {
+
+
+    @Mock
+    MainActivity mainActivity;
+
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.mrides", appContext.getPackageName());
+    }
+}
+
+
