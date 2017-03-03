457d19b7e02881380558e7c02786b9b2e0fd9124
Harrison <harrison.a101@hotmail.com>
2017-03-03 01:54:14 -0500

    fixed log in with multiple accounts bug. however email not recieved.

---
+++ client/app/src/main/java/DirectionModel/IPersistanceObject.java
@@ -0,0 +1,8 @@
+package DirectionModel;
+
+/**
+ * Created by Deez Nuts on 3/2/2017.
+ */
+
+public interface IPersistanceObject {
+}
---
+++ client/app/src/main/java/com/example/mrides/controller/Controller.java
@@ -0,0 +1,8 @@
+package com.example.mrides.controller;
+
+/**
+ * Created by Deez Nuts on 2/28/2017.
+ */
+
+public class Controller {
+}
