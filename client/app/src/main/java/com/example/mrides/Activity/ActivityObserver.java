b4f44cedbe5504a737f1e642c07dbb29782e1519
Harrison <harrison.a101@hotmail.com>
2017-03-04 14:51:39 -0500

    Added required for Subject interface for the Observer patter, so that the requesthandler can implement these messages to notify the activities that the response has been recieved.

---
+++ client/app/src/main/java/com/example/mrides/Activity/ActivityObserver.java
@@ -0,0 +1,8 @@
+package com.example.mrides.Activity;
+
+
+
+public interface ActivityObserver {
+
+    public void responseReceived();
+}
--- client/app/src/main/java/com/example/mrides/controller/Subject.java
+++ client/app/src/main/java/com/example/mrides/controller/Subject.java
@@ -1,5 +1,7 @@
 package com.example.mrides.controller;
 
+import com.example.mrides.Activity.ActivityObserver;
+
 import java.util.ArrayList;
 
 /**
@@ -7,5 +9,10 @@ import java.util.ArrayList;
  */
 
 public interface Subject {
+    ArrayList<ActivityObserver> observers = new ArrayList<>();
+
+    public void attach(ActivityObserver observerToAdd);
+    public void detach(ActivityObserver observerToRemove);
+    public void Notify();
 
 }
