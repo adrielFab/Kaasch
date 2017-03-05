7810332e94ce23552b6af1f663b7a8682b49cb14
Harrison <harrison.a101@hotmail.com>
2017-03-04 22:37:20 -0500

    IK-91 created method to check wifi in abstract activity which will be inherited by other activities.

--- client/app/src/main/java/com/example/mrides/Activity/DefaultActivity.java
+++ client/app/src/main/java/com/example/mrides/Activity/DefaultActivity.java
@@ -1,10 +1,22 @@
 package com.example.mrides.Activity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Deez Nuts on 3/4/2017.
- */
 
 public abstract class DefaultActivity extends AppCompatActivity{
+
+    public void checkWifi(){
+        ConnectivityManager cm =
+                (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        if(!isConnected){
+            
+        }
+    }
 }
