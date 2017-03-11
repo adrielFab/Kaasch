48ede9750527fa7d78b179dca8c7ed561ad27f72
Harrison <harrison.a101@hotmail.com>
2017-03-10 23:12:55 -0500

    IK-91 Created Firebase service classes which handle sending messages and recieving notifications.

---
+++ client/app/src/main/java/com/example/mrides/Notification/FirebaseInstanceIdService.java
@@ -0,0 +1,8 @@
+package com.example.mrides.Notification;
+
+/**
+ * Created by Deez Nuts on 3/10/2017.
+ */
+
+public class FirebaseInstanceIdService {
+}
---
+++ client/app/src/main/java/com/example/mrides/Notification/FirebaseMessagingService.java
@@ -0,0 +1,37 @@
+package com.example.mrides.Notification;
+
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ *
+ */
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // ...
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        System.out.println("From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            System.out.println("Message data payload: " + remoteMessage.getData());
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            System.out.println("Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+
+
+}
