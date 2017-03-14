bfd3d7436d798e7054a4b4ebcbfa7e532f186266
Harrison <harrison.a101@hotmail.com>
2017-03-14 08:29:53 -0400

    Revert "Merge branch 'IK-46'"
    This reverts commit 7ab659cf341b926582c5cfada2ecd43dafb33fdd, reversing
    changes made to 8991fa66674c2f3e87b32169b02cd62bdf96332b.

--- client/README.md
+++
@@ -1,2 +0,0 @@
-# Kaasch
-This layer is the application layer of the code. It contains all the code which will be stored on the users phone. 
--- client/app/src/main/java/com/example/mrides/Activity/ActivityObserver.java
+++
@@ -1,8 +0,0 @@
-package com.example.mrides.Activity;
-
-
-
-public interface ActivityObserver {
-
-    public void responseReceived(String response);
-}
