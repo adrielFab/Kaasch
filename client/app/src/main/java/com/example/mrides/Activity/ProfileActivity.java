e8e7e997637d2b5a5aed4c7104cf672d919e3f48
Harrison <harrison.a101@hotmail.com>
2017-03-13 20:28:01 -0400

    IK-60 ProfileActivity now implements ActivityObserver. Additionally the webserver ip is added to the resources.

--- client/app/src/main/java/com/example/mrides/Activity/ProfileActivity.java
+++ client/app/src/main/java/com/example/mrides/Activity/ProfileActivity.java
@@ -2,14 +2,27 @@ package com.example.mrides.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.mrides.R;
+import com.example.mrides.controller.RequestHandler;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements ActivityObserver{
 
+    private RequestHandler requestHandler = new RequestHandler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void sendInvite(View view){
+        requestHandler.attach(this);
+    }
+
+    @Override
+    public void responseReceived(String response) {
+        requestHandler.detach(this);
+        //requestHandler.httpPostStringRequest(getString(R.string.web_server_ip),);
     }
 }
