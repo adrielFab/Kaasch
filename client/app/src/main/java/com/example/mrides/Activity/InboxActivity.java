8f5c69cbcd37ab82427158a87dadb10d005de143
Harrison <harrison.a101@hotmail.com>
2017-03-16 19:14:30 -0400

    IK-60 Added RecyclerView widget to the InboxActivity to support mutli list view.

--- client/app/src/main/java/com/example/mrides/Activity/InboxActivity.java
+++ client/app/src/main/java/com/example/mrides/Activity/InboxActivity.java
@@ -1,15 +1,40 @@
 package com.example.mrides.Activity;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mrides.R;
+import com.example.mrides.controller.RequestHandler;
 
 public class InboxActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private RequestHandler requestHandler = new RequestHandler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }
 }
