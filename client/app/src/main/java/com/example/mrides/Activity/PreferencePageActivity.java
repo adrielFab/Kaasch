a65474d1613891e04b6372edb4f8b455eca518bf
Adriel Fabella <adriel.fab@gmail.com>
2017-03-15 21:03:50 -0400

    IK-97 Refactored the preference page so that it will only be 1 page.
    refacotred the if statement into a switch statement

--- client/app/src/main/java/com/example/mrides/Activity/PreferencePageActivity.java
+++ client/app/src/main/java/com/example/mrides/Activity/PreferencePageActivity.java
@@ -9,13 +9,16 @@ package com.example.mrides.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.example.mrides.R;
 
@@ -24,6 +27,7 @@ import java.util.Calendar;
 public class PreferencePageActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button btnDatePicker, btnTimePicker;
+    private ImageView btnSmokePref, btnMalePref, btnFemalePref;
     private EditText txtDate, txtTime;
 
     private int mYear, mMonth, mDay, mHour, mMinute;
@@ -43,6 +47,11 @@ public class PreferencePageActivity extends AppCompatActivity implements View.On
         btnDatePicker.setOnClickListener(this);
         btnTimePicker.setOnClickListener(this);
 
+        btnSmokePref = (ImageView) findViewById(R.id.img_smoke);
+        btnMalePref = (ImageView) findViewById(R.id.img_boy);
+        btnFemalePref = (ImageView) findViewById(R.id.img_girl);
+
+
 
     }
 
@@ -55,47 +64,48 @@ public class PreferencePageActivity extends AppCompatActivity implements View.On
     @Override
     public void onClick(View v) {
 
-        if (v == btnDatePicker) {
+        switch (v.getId()) {
+            case R.id.btn_date:
+                // Get Current Date
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
 
-            // Get Current Date
-            final Calendar c = Calendar.getInstance();
-            mYear = c.get(Calendar.YEAR);
-            mMonth = c.get(Calendar.MONTH);
-            mDay = c.get(Calendar.DAY_OF_MONTH);
 
+                DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                        new DatePickerDialog.OnDateSetListener() {
 
-            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                    new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
 
-                        @Override
-                        public void onDateSet(DatePicker view, int year,
-                                              int monthOfYear, int dayOfMonth) {
+                                txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
 
-                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+                break;
 
-                        }
-                    }, mYear, mMonth, mDay);
-            datePickerDialog.show();
+            case R.id.btn_time:
+                // Get Current Time
+                final Calendar ct = Calendar.getInstance();
+                mHour = ct.get(Calendar.HOUR_OF_DAY);
+                mMinute = ct.get(Calendar.MINUTE);
+
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+
+                                txtTime.setText(hourOfDay + ":" + minute);
+                            }
+                        }, mHour, mMinute, false);
+                timePickerDialog.show();
         }
-        if (v == btnTimePicker) {
 
-            // Get Current Time
-            final Calendar c = Calendar.getInstance();
-            mHour = c.get(Calendar.HOUR_OF_DAY);
-            mMinute = c.get(Calendar.MINUTE);
-
-            // Launch Time Picker Dialog
-            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
-                    new TimePickerDialog.OnTimeSetListener() {
-
-                        @Override
-                        public void onTimeSet(TimePicker view, int hourOfDay,
-                                              int minute) {
-
-                            txtTime.setText(hourOfDay + ":" + minute);
-                        }
-                    }, mHour, mMinute, false);
-            timePickerDialog.show();
-        }
     }
 }
