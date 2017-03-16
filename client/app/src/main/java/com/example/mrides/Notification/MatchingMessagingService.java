757d9e2f6717e8b4ec6424a8ca8f9b5c343b7406
Harrison <harrison.a101@hotmail.com>
2017-03-16 17:32:21 -0400

    IK-60 Added permision in AndroidManifest to allow notifications to be recieved even when the app is closed. Additionally when notification is recieved user is brought to inbox page.

--- client/app/src/main/java/com/example/mrides/Notification/MatchingMessagingService.java
+++ client/app/src/main/java/com/example/mrides/Notification/MatchingMessagingService.java
@@ -5,10 +5,12 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
+import com.example.mrides.Activity.InboxActivity;
 import com.example.mrides.Activity.MainActivity;
 import com.example.mrides.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -40,17 +42,19 @@ public class MatchingMessagingService extends FirebaseMessagingService {
         if (remoteMessage.getNotification() != null) {
             System.out.println("Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
-
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(500);
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, InboxActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
         notificationBuilder.setContentTitle("FCM NOTIFICATION");
         notificationBuilder.setContentText(remoteMessage.getNotification().getBody());
         notificationBuilder.setAutoCancel(true);
-        notificationBuilder.setSmallIcon(R.mipmap.ic_launcher);
+        notificationBuilder.setSmallIcon(R.mipmap.icon);
         notificationBuilder.setContentIntent(pendingIntent);
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0,notificationBuilder.build());
