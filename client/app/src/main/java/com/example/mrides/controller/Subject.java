340d82de1bc2030bd62bfa18889e5895cab3e9b1
Harrison <harrison.a101@hotmail.com>
2017-03-18 15:36:54 -0400

    [Documentation] added javadoc.

--- client/app/src/main/java/com/example/mrides/controller/Subject.java
+++ client/app/src/main/java/com/example/mrides/controller/Subject.java
@@ -7,8 +7,24 @@ import java.util.ArrayList;
 import DirectionModel.IPersistanceObject;
 
 public interface Subject {
+
+    /**
+     * This method is designed to attah observers to a list of observers on the subject class
+     * @param observerToAdd The observer to add to the concrete subject
+     */
     public void attach(ActivityObserver observerToAdd);
+
+    /**
+     * When an observer no longer needs to listen to a subject, the subject needs to remove
+     * the obsever reference from the class array
+     * @param observerToRemove This is the observer to remove from the list of observers
+     */
     public void detach(ActivityObserver observerToRemove);
+
+    /**
+     * Tis method notifies all observers that the state of the subject has changed.
+     * @param response
+     */
     public void Notify(String response);
 
 }
