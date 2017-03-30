5507143bc7444336f331c0d8690d1f8a075b71a6
Adriel Fabella <adriel.fab@gmail.com>
2017-03-30 03:00:09 -0400

    IK-112 Refactored preference page to handle both types of createRoute.
    Choosing driver or passenger will bring them to their corresponding page

--- client/app/src/main/java/com/example/mrides/Activity/CreateRouteDriverActivity.java
+++ client/app/src/main/java/com/example/mrides/Activity/CreateRouteDriverActivity.java
@@ -1,13 +1,6 @@
 package com.example.mrides.Activity;
 
-import android.os.Bundle;
-import com.example.mrides.R;
-
 public class CreateRouteDriverActivity extends CreateRouteActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_route_driver);
-    }
+
 }
