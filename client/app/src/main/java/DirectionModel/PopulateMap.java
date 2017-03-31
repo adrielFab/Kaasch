8aa3feada8dd5944c2b15a906261d7be334c4880
Harrison <harrison.a101@hotmail.com>
2017-03-31 16:44:28 -0400

    IK-114 [Refactored] Converted PopulateMap class from an AsyncTask to an ActivityObserver.

--- client/app/src/main/java/DirectionModel/PopulateMap.java
+++ client/app/src/main/java/DirectionModel/PopulateMap.java
@@ -5,27 +5,22 @@
 */
 package DirectionModel;
 
-import android.os.AsyncTask;
+import android.content.Context;
+import com.example.mrides.Activity.ActivityObserver;
 import com.example.mrides.Activity.CreateRouteActivity;
+import com.example.mrides.controller.RequestHandler;
 import com.example.mrides.userDomain.User;
 import com.google.android.gms.maps.model.LatLng;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
-public class PopulateMap extends AsyncTask<Void, Void, String>{
+public class PopulateMap implements ActivityObserver{
 
     private CreateRouteActivity createRouteActivity;
     private ArrayList<User> usersOnMapCatalog = new ArrayList<>();
-
+    private RequestHandler requestHandler = new RequestHandler();
 
     public PopulateMap(CreateRouteActivity createRouteActivity){
         this.createRouteActivity = createRouteActivity;
@@ -50,75 +45,13 @@ public class PopulateMap extends AsyncTask<Void, Void, String>{
     }
 
     /**
-     * Method that establishes a connection with the web server, sends a GET request
-     * and retrieves the corresponding data
-     * @param voids
-     * @return String This returns the response of the request
+     *  This method requests the users from our server to be added to usersOnMapCatalog
+     * @param context the context of the activty making the request
      */
-    @Override
-    protected String doInBackground(Void... voids) {
-
+    public void requestUsers(Context context) {
         String retrieve_url = "http://successdrivingschool.ca/test2_android.php";
-
-        try {
-
-            URL url = new URL(retrieve_url);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("GET");
-            httpURLConnection.setDoOutput(true);
-            httpURLConnection.setDoInput(true);
-
-            InputStream inputStream =  httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
-            String result = "";
-            String line = "";
-            while ((line = bufferedReader.readLine()) != null){
-
-                result += line;
-            }
-            bufferedReader.close();
-            inputStream.close();
-            httpURLConnection.disconnect();
-
-            return result;
-
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-        } catch (IOException e) {
-
-            e.printStackTrace();
-        }
-
-        return null;
-
-    }
-
-    /**
-     * This method is executed before doInBackground
-     */
-    @Override
-    protected void onPreExecute() {
-
-        super.onPreExecute();
-    }
-
-    /**
-     * This method is executed after doInBackground. If the result is not a successful
-     * JSON string, the method will display an error message.
-     * @param result
-     */
-    @Override
-    protected void onPostExecute(String result) {
-
-        try{
-
-            parseUserandMarker(result);
-        } catch (JSONException e){
-
-            e.printStackTrace();
-        }
-
+        requestHandler.attach(this);
+        requestHandler.httpGetStringRequest(retrieve_url,context);
     }
 
     /**
@@ -169,5 +102,15 @@ public class PopulateMap extends AsyncTask<Void, Void, String>{
         }
 
         this.createRouteActivity.populateGoogleMap();
+    }
+
+    @Override
+    public void Update(String response) {
+        requestHandler.detach(this);
+        try {
+            parseUserandMarker(response);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
