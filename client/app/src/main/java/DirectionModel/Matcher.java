fc6d588c8ffc76d3214cc327cfacaad22c33174a
An Ran Chen <anr_chen@live.concordia.ca>
2017-04-03 20:38:57 -0400

    IK-115[Refactor] Added spacing after bracket
    Fixed to conform to the coding convention

--- client/app/src/main/java/DirectionModel/Matcher.java
+++ client/app/src/main/java/DirectionModel/Matcher.java
@@ -20,28 +20,28 @@ public class Matcher {
     private ArrayList <User> userOnMapCatalog = new ArrayList<>();
     private HashMap<Integer, Marker> matchedMarkers = new HashMap<>();
 
-    public Matcher(Route route){
+    public Matcher(Route route) {
         this.route = route;
     }
 
-    public void setMatchedMarkers(HashMap<Integer, Marker> matchedMarkers){
+    public void setMatchedMarkers(HashMap<Integer, Marker> matchedMarkers) {
         this.matchedMarkers = matchedMarkers;
     }
 
-    public HashMap<Integer, Marker> getMatchedMarkers(){
+    public HashMap<Integer, Marker> getMatchedMarkers() {
         return this.matchedMarkers;
     }
 
-    public void setUserMapCatalog(ArrayList <User> userOnMapCatalog){
+    public void setUserMapCatalog(ArrayList <User> userOnMapCatalog) {
         this.userOnMapCatalog = userOnMapCatalog;
     }
 
-    public ArrayList <User> getUserMapCatalog(){
+    public ArrayList <User> getUserMapCatalog() { 
         return this.userOnMapCatalog;
     }
 
     /** calculates the distance between two locations in MILES */
-    private double distance(double lat1, double lng1, double lat2, double lng2) {
+    private double distance(double lat1, double lng1, double lat2, double lng2) { 
 
         double earthRadius = 3958.75; // in miles, change to 6371 for kilometer output
         double dLat = Math.toRadians(lat2-lat1);
@@ -63,7 +63,7 @@ public class Matcher {
      * @param userLocation
      * @return boolean
      */
-    public boolean validateDistance(LatLng passengerLocation, LatLng userLocation) {
+    public boolean validateDistance(LatLng passengerLocation, LatLng userLocation) { 
         if (distance( passengerLocation.latitude, passengerLocation.longitude,
                 userLocation.latitude, userLocation.longitude) <= 0.1) {
             return true;
@@ -100,7 +100,7 @@ public class Matcher {
     }
 
     private void matchDistance(int i, int passengerRouteId, int dateMatched, List<LatLng> routeOfUser,
-                               boolean pickUpBool, boolean goToEnd, LatLng pickUp, LatLng drop){
+                               boolean pickUpBool, boolean goToEnd, LatLng pickUp, LatLng drop) {
         while (i < routeOfUser.size() && !pickUpBool) {
             LatLng pointInPoly = routeOfUser.get(i);
             if (this.validateDistance(pickUp, pointInPoly) && !goToEnd && dateMatched==0) {
