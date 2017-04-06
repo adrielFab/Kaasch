5b1b441314b47bf3bec11cab0b3a0c702ae8295f
Adriel Fabella <adriel.fab@gmail.com>
2017-04-06 12:57:00 -0400

    IK-133 A translation animation was added for the car imageview

--- client/app/src/main/java/com/example/mrides/Activity/FirstTimeActivity.java
+++ client/app/src/main/java/com/example/mrides/Activity/FirstTimeActivity.java
@@ -2,6 +2,7 @@ package com.example.mrides.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.mrides.R;
 
@@ -11,5 +12,8 @@ public class FirstTimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_time);
+
+        ImageView car = (ImageView) findViewById(R.id.imageViewCar);
+        car.animate().translationXBy(-3000f).setDuration(4000);
     }
 }
