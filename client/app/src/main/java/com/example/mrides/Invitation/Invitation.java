eabe1d9c0b20624c86ec9f90ae6d5a89004c7aa8
Harrison <harrison.a101@hotmail.com>
2017-04-07 17:01:54 -0400

    IK-137 Added logged in user and passenger to invitation class.

---
+++ client/app/src/main/java/com/example/mrides/Invitation/Invitation.java
@@ -0,0 +1,12 @@
+package com.example.mrides.Invitation;
+
+import com.example.mrides.userDomain.Passenger;
+import com.example.mrides.userDomain.User;
+
+public class Invitation {
+
+    private String driverRouteName;
+    private User driver;
+    private Passenger passenger;
+
+}
